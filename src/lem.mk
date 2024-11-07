7a9c7781a08670fa2f684063705f881f7e619ce7
Matej Urban <maturvo@gmail.com>
2024-11-07 19:01:34 +0000

    refactor abstract relocations

--- src/lem.mk
+++ src/lem.mk
@@ -56,6 +56,7 @@ LEM_ELF_SRC := byte_sequence.lem byte_pattern.lem byte_pattern_extra.lem \
 	elf_relocation.lem \
 	elf_interpreted_segment.lem elf_interpreted_section.lem \
 	elf_note.lem elf_file.lem elf_dynamic.lem \
+	elf_symbolic.lem \
 	dwarf_ctypes.lem dwarf.lem ldconfig.lem
 
 LEM_ABI_SRC := \
